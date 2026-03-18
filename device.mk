0894f125b8ec1fc1bc8adae1ada7478e4e69ae42
OpenSource Guy <228935929+os-guy-original@users.noreply.github.com>
2026-03-18 22:50:19 +0300

    Fixed the mssing seperator at the line 18

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@ RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/bootctl
 
 # Boot control, Firmware
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti
+    android.hardware.boot@1.2-impl-qti \
     android.hardware.boot@1.2-impl-qti.recovery 
 
 PRODUCT_PACKAGES += \
