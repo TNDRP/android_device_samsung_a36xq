3e01e084ddc6e4b860936ebe2c1902774af1cff7
OpenSource Guy <228935929+os-guy-original@users.noreply.github.com>
2026-03-19 00:56:07 +0300

    Remove system_dlkm from partition lists for OFOX compatibility
    system_dlkm is not a valid partition in OrangeFox build system.
    Valid partitions: system, vendor, product, system_ext, odm, vendor_dlkm, odm_dlkm

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,6 @@ DEVICE_PATH := device/samsung/a36xq
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
-    system_dlkm \
     vendor \
     dtbo \
     init_boot \
@@ -97,7 +96,6 @@ BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 134217728
 BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := \
-    system_dlkm \
     vendor \
     odm \
     system \
